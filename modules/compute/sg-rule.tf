9ffa0e68b5bacaba03fa6b68f457787e62758559
Jose Almino <jose.araujo@compasso.com.br>
2024-06-19 15:08:38 -0300

    update sg include infress https

--- modules/compute/sg-rule.tf
+++ modules/compute/sg-rule.tf
@@ -38,6 +38,16 @@ resource "aws_security_group_rule" "app_sg_ingress_ssh" {
   security_group_id = aws_security_group.app_sg.id
 }
 
+
+resource "aws_security_group_rule" "app_sg_ingress_ssh" {
+  type              = "ingress"
+  from_port         = 443
+  to_port           = 443
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.app_sg.id
+}
+
 resource "aws_security_group_rule" "app_sg_egress" {
   type              = "egress"
   from_port         = 0
